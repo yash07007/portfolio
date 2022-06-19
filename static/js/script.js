3b2ab15951eb674f43acdb90591addb662ebd75c
yash07007 <yashsolanki777@gmail.com>
2022-06-19 13:29:35 -0700

    feat: implement portfolio section

--- static/js/script.js
+++ static/js/script.js
@@ -12,3 +12,44 @@ tabButtons.addEventListener("click", function (e) {
         aboutSection.querySelector(targetTab).classList.add("active");
     }
 });
+
+// Portfolio Item Modal
+
+document.addEventListener("click", function (e) {
+    // Show Modal in button click
+    if (e.target.classList.contains("view-project-button")) {
+        togglePortfolioModal();
+        document.querySelector(".portfolio-modal").scrollTo(0, 0);
+        populatePortfolioModal(e.target.parentElement);
+    }
+
+    // Hide Modal on offscreen click
+    if (e.target.classList.contains("modal-inner")) {
+        togglePortfolioModal();
+    }
+});
+
+function togglePortfolioModal() {
+    const portfolioModal = document.querySelector(".portfolio-modal");
+    portfolioModal.classList.toggle("open");
+    document.body.classList.toggle("no-scroll");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+
+document.querySelector(".modal-close").addEventListener("click", togglePortfolioModal);
+
+// Hide Modal on ESC key press
+document.addEventListener("keyup", function (e) {
+    if (e.key === "Escape") {
+        togglePortfolioModal();
+    }
+});
+
+function populatePortfolioModal(portfolioItem) {
+    const modalHeader = document.querySelector(".modal-header h3");
+    const modalBody = document.querySelector(".modal-body");
+    const modalImage = document.querySelector(".modal-thumbnail img");
+    modalHeader.innerHTML = portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+    modalBody.innerHTML = portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+    modalImage.src = portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+}
