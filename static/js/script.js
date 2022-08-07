5801cd33e354ce4cc6d02df4514b79996a3fffee
yash07007 <yashsolanki777@gmail.com>
2022-08-07 10:20:59 -0700

    feat: implement overlay guard

--- static/js/script.js
+++ static/js/script.js
@@ -19,6 +19,7 @@ function toggleNavbar() {
 
 document.addEventListener("click", (e) => {
     if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+        document.querySelector(".overlay").classList.add("active");
         navToggler.classList.add("hide");
         if (e.target.classList.contains("nav-item")) {
             toggleNavbar();
@@ -32,6 +33,7 @@ document.addEventListener("click", (e) => {
             window.scrollTo(0, 0);
             document.body.classList.remove("hide-scrolling");
             navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
         }, 500);
     }
 });
