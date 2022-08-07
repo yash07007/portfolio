2501ee163df3458b252c8290418bced77ce9b034
yash07007 <yashsolanki777@gmail.com>
2022-08-07 10:14:23 -0700

    feat: implement header section

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,42 @@
-// About Tabs
+// Toggle Navbar
 
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+});
+
+function hideSection() {
+    document.querySelector("section.active").classList.toggle("fade-out");
+}
+
+function toggleNavbar() {
+    document.querySelector(".header").classList.toggle("active");
+}
+
+// Active Section
+
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+        navToggler.classList.add("hide");
+        if (e.target.classList.contains("nav-item")) {
+            toggleNavbar();
+        } else {
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(() => {
+            document.querySelector("section.active").classList.remove("active", "fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0, 0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+        }, 500);
+    }
+});
+
+// About Tabs
 const aboutSection = document.querySelector(".about-section");
 const tabButtons = document.querySelector(".about-tabs");
 tabButtons.addEventListener("click", function (e) {
@@ -14,7 +51,6 @@ tabButtons.addEventListener("click", function (e) {
 });
 
 // Portfolio Item Modal
-
 document.addEventListener("click", function (e) {
     // Show Modal in button click
     if (e.target.classList.contains("view-project-button")) {
